fb29f5b5246b0563572ce3610814c17df9922481
govardhana_7 <chintuilla1719@gmail.com>
2026-06-10 08:20:27 +0530

    Fix ESLint configuration and remove unused imports

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
-const dns = require('dns').promises;
 
 const app = express();
 
